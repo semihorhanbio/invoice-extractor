9e2aeba79e4e1a3e9116d290b58757bad177f47e
Semih <50242428+semihorhanbio@users.noreply.github.com>
2024-07-25 12:38:06 +0300

    change ocr to openai api

--- ocr.py
+++ ocr.py
@@ -1,22 +1,61 @@
 from fastapi import APIRouter, File, UploadFile, HTTPException, status
 from fastapi.responses import JSONResponse
-import pytesseract
+#import pytesseract
 import json
+import base64
+import requests
 from utils import extract_pdf, save_file_to_server
 from groq import Groq
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 groq_api_key = os.getenv("GROQ_API_KEY")
 
 router = APIRouter()
 groq_client = Groq(api_key=groq_api_key)
 
+# Function to encode the image
+def encode_image(image):
+    return base64.b64encode(image.read()).decode('utf-8')
+
+def call_openai_api(image_base64):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4o",
+        "response_format": { "type": "json_object" },
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You are a specialist in comprehending invoices. Input files in the form of invoices will be provided to you, and your task is to convert invoice data into json format. If the requirement information is not found, set "no value". The required properties in JSON object are as follows.\n  Invoice = {"customer_name": str, "customer_vkn": int, customer_tckn": int, "vendor_name": str, "vendor_vkn": int, vendor_tckn": int, invoice_senario: str, invoice_type: str, invoice_no: str, invoice_date: date, total_of_goods_and_services: float, calculated_vat: float, invoice_total: float, invoice_lines: list[goods_and_services_name: str, goods_and_services_quantity: int, goods_and_services_unit_price: float, goods_and_services_vat_rate: float, goods_and_services_total: float]}\n  Return just a `Invoice` in json format no additional info."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image_base64}",
+                            "detail": "low"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 1000
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    return response.json()
 
 def invoke_ocr(img_path: str) -> str:
     """
-    Extract text from an image using EasyOCR
+    Extract text from an image using tesseract
     """
     try:
         text = pytesseract.image_to_string(img_path)
@@ -31,8 +70,10 @@ async def inference(file: UploadFile = File(...)):
     if file:
         if file.content_type in ["image/jpeg", "image/jpg", "image/png"]:
             print("Images Uploaded: ", file.filename)
-            temp_file = save_file_to_server(file, path="./", save_as=file.filename)
-            text = invoke_ocr(temp_file)
+            image_base64 = encode_image(file)
+            response = call_openai_api(image_base64)
+            result = json.loads( response.choices[0].message.content)
+            return JSONResponse(content=result, media_type="application/json")
         elif file.content_type == "application/pdf":
             pdf_bytes = await file.read()
             text = extract_pdf(pdf_bytes)
