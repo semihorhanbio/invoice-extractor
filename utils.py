aa0cde63be99ad7285afa55cb625636f294012b4
Semih <50242428+semihorhanbio@users.noreply.github.com>
2024-07-25 09:54:27 +0000

    move functions to utils

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import fitz  # pymupdf
 import shutil
 import os
+import base64
+import requests
 
 
 def save_file_to_server(uploaded_file, path=".", save_as="default"):
@@ -25,3 +27,46 @@ def extract_pdf(file_bytes):
         for page in doc:
             text += page.get_text()
         return text
+
+# Function to encode the image
+def encode_image(image):
+    return base64.b64encode(image.read()).decode('utf-8')
+
+def call_openai_api(image_base64, openai_api_key):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_api_key}"
+    }
+    payload = {
+    "model": "gpt-4o",
+    "response_format": { "type": "json_object" },
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": "What’s in this image?"
+            },
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{image_base64}",
+                "detail": "low"
+            }
+            }
+        ]
+        }
+    ],
+    "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    return response.json()
+
+""" def invoke_ocr(img_path: str) -> str:
+    try:
+        text = pytesseract.image_to_string(img_path)
+        return text
+    except:
+        return "[ERROR] Unable to process file: {0}".format(img_path) """
